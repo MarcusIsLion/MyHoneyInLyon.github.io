6698d3950ae3a3dc1ea73b25f7902ddd16d2df32
MarcusIsLion <vmathis934@gmail.com>
2021-12-31 11:24:42 +0100

    innit commit
    -ajout de l'index (sans texte)
    -ajout de style.css (sans texte)
    -ajout de BDN.html (sans texte)

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8"/>
+    <meta name="keywords" content="MyHoneyInLyon, miel de lyon, vente de miel, mon miel chez moi">
+    <meta name="copyright" content="MyHoneyInLyon">
+    <meta name="author" content="MyHoneyInLyon">
+    <meta name="generator" content="Visual Studio Code">
+    <meta name="publisher" content="MyHoneyInLyon">
+    <link rel="icon" href="         ">
+    <link rel="apple-touch-icon" href="         ">
+    <link rel="stylesheet" href="style.css" />
+    <title>Accueil MyHoneyInLyon</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link href="fontawesome/css/all.css" rel="stylesheet">
+    <!-- <title> et </title> correspond au nom de l'onglet. -->
+</head>*
+
+
+<body>
+<?php
+    include("");
+?>
+
+
+
+</body>
